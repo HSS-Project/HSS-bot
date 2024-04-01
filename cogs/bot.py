282d79e20339760626be7126abbbb6edcc09835d
kazuma1112 <88960435+kazuma11121125@users.noreply.github.com>
2024-04-01 17:30:26 +0900

    [fix] コメントアウトしたらいけないとこをしていた

--- cogs/bot.py
+++ cogs/bot.py
@@ -26,11 +26,11 @@ class bot_(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # @app_commands.command(name="register", description="学校へのアクセス権を追加します。")
-    # async def register(self, interaction:discord.Interaction):
-    #     view = discord.ui.View()
-    #     view.add_item(discord.ui.Button(label="アクセスする",style=discord.ButtonStyle.url, url="https://hss.aknet.tech/application/6392922062414939136"))
-    #     await interaction.response.send_message(embed=discord.Embed(title="登録", description="下のボタンから、Botを学校に追加してください。"), view=view, ephemeral=True)
+    @app_commands.command(name="register", description="学校へのアクセス権を追加します。")
+    async def register(self, interaction:discord.Interaction):
+        view = discord.ui.View()
+        view.add_item(discord.ui.Button(label="アクセスする",style=discord.ButtonStyle.url, url="https://hss.aknet.tech/application/6418796904833876992"))
+        await interaction.response.send_message(embed=discord.Embed(title="登録", description="下のボタンから、Botを学校に追加してください。"), view=view, ephemeral=True)
 
     @app_commands.command(name="dashboard",description="ダッシュボードにアクセスします。")
     async def dashboard(self, interaction: discord.Interaction):
