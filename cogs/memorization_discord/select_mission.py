54ad470316fa082bc8b9e425f69b62d5b9c48175
kazuma1112 <kazuma1112.mainmail@gmail.com>
2024-06-20 12:56:58 +0900

    [fix] titleの変更忘れ

--- cogs/memorization_discord/select_mission.py
+++ cogs/memorization_discord/select_mission.py
@@ -51,8 +51,9 @@ class SelectMission_4(discord.ui.Select):
         await SelectMiView(interaction,self.title,int(self.values[0]),self.mode).select_response()
     
 class SelectMissionView(discord.ui.View):
-    def __init__(self,titles,missions:list,mode):
+    def __init__(self,title,missions:list,mode):
         super().__init__()
+        self.title = title
         missions_len = len(missions)
         self.missions = missions
         self.mode = mode
@@ -71,13 +72,13 @@ class SelectMissionView(discord.ui.View):
                         break
                     self.options_list[j].append(discord.SelectOption(label=missions[i], value=str(i)))
                     
-        self.add_item(SelectMission_1(self.options_1,self.mode))
+        self.add_item(SelectMission_1(self.title,self.options_1,self.mode))
         if missions_len > 25:
-            self.add_item(SelectMission_2(self.options_2,self.mode))
+            self.add_item(SelectMission_2(self.title,self.options_2,self.mode))
         if missions_len > 50:
-            self.add_item(SelectMission_3(self.options_3,self.mode))
+            self.add_item(SelectMission_3(self.title,self.options_3,self.mode))
         if missions_len > 75:
-            self.add_item(SelectMission_4(self.options_4,self.mode))
+            self.add_item(SelectMission_4(self.title,self.options_4,self.mode))
                 
 class SelectMiView:
     def __init__(self,intracton:discord.Interaction,title,selectnumber,mode):
@@ -88,4 +89,4 @@ class SelectMiView:
         
     async def select_response(self):
         if self.mode == 0:
-            await Delete().delete_misson_select(str(self.intracton.user.id),self.selectnumber)
+            await Delete().delete_misson_select(str(self.intracton.user.id),self.title,self.selectnumber)
